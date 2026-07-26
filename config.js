a1027820e18743a0cdb225c6ca96d87f39bfa311
michaelskinner75-cyber <michaelskinner75@gmail.com>
2026-07-26 21:20:26 +0100

    Fix Supabase project URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Public Supabase connection details for the photography website.
 window.PHOTO_SITE_CONFIG = {
-  supabaseUrl: "https://tmagekpcrnaafhpqqrfc.supabase.co",
+  supabaseUrl: "https://ltmagekpcrnaafhpqqrfc.supabase.co",
   supabaseAnonKey: "sb_publishable_akRSKXr8dId8mfZFYo61rQ_cISoX1Dl"
 };
